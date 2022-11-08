fa5fd1f9f018b6345d8af300d760d1dafcfd01b3
prado156 <ppattem@purdue.edu>
2022-11-08 16:36:15 -0500

    fixing redirects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import ViewBlogs from "./pages/ViewBlogs";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div></div>;
+  return (
+    <div>
+      {/* <h1 className="text-xl font-bold underline">Hello world!</h1> */}
+      <ViewBlogs />
+    </div>
+  );
 }
 
 export default App;
