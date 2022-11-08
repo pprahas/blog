1935cf530bf857157a3f6a570fc7da439c2c3f77
prado156 <ppattem@purdue.edu>
2022-11-08 15:32:35 -0500

    setup done

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("HW7 db connected"))
+  .catch((err) => console.log("HW7 db not connected"));
+
+app.use(morgan("dev"));
+app.use(cors({ origin: true, credentials: true }));
+
+const port = 9090;
+
+const server = app.listen(port, () => console.log("Server is running"));
